f2fb0dc83c6cd168174b770d913ee57c5acc5fac
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2020-06-23 05:49:01 +0530

    tried Mondrian Layout of Images

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,29 +1,36 @@
 const log = console.log;
-
+const imgContainers = document.querySelectorAll('.gallery-container .gallery .img-container');
+const imgs = document.querySelectorAll('.gallery-container .gallery .img-container .image');
 let update_transform_origin = (event) => {
     let rect = event.target.getBoundingClientRect();
     xp = event.offsetX / rect.width * 100;
     yp = event.offsetY / rect.height * 100;
     event.target.style.transformOrigin = `${xp}% ${yp}%`;
 }
+let maxHeightOfImage = 0;
+let maxWidthOfImage = 0;
 
-document.querySelectorAll('.gallery-container .gallery .img-container')
-    .forEach(img_container => {
-        img_container.addEventListener('mouseover', update_transform_origin);
-        img_container.addEventListener('mousemove', update_transform_origin);
-    });
+imgContainers.forEach(img_container => {
+    img_container.addEventListener('mouseover', update_transform_origin);
+    img_container.addEventListener('mousemove', update_transform_origin);
+});
 
-document.querySelectorAll('.gallery-container .gallery .img-container .image')
-    .forEach(img => {
-        let wBh = img.naturalWidth/img.naturalHeight;
-        let hBw = img.naturalHeight/img.naturalWidth;
-        // if(wBh > hBw) {
-        //     // img.parentElement.style.gridColumn = `span ${Math.round(4*wBh)} / auto`;
-        //     log(`column: span ${Math.round(4*wBh)} / auto`);
-        // } else if (hBw > wBh) {
-        //     img.parentElement.style.gridRow = `span ${Math.round(4*hBw)} / auto`;
-        //     log(`Row: span ${Math.round(4*wBh)} / auto`);
-        // }
-        
-        img.parentElement.style.gridRow = `span ${Math.round(4*hBw)} / auto`;
-    });
+imgs.forEach(img => {
+    if(img.naturalWidth > maxWidthOfImage) {
+        maxWidthOfImage = img.naturalWidth;
+    }
+    if(img.naturalHeight > maxHeightOfImage) {
+        maxHeightOfImage = img.naturalHeight;
+    }
+})
+
+imgs.forEach(img => {
+    let wBh = img.naturalWidth / img.naturalHeight;
+    let hBw = img.naturalHeight / img.naturalWidth;
+
+    img.parentElement.style.gridColumn = `span ${Math.round(wBh)}`;
+
+    img.parentElement.style.gridRow = `span ${Math.round(hBw)}`;
+
+    // img.parentElement.style.gridRow = `span ${Math.round(4*hBw)} / auto`;
+});
